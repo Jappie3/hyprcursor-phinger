01b5429e6a1b72214c30f5032af23dd00d9affdf
Jappie3 <jasper22034@gmail.com>
2024-05-13 00:04:02 +0200

    feat: package SVGs from upstream repo instead of grabbing the release tarball w/ XCursor theme

--- nix/package.nix
+++ nix/package.nix
@@ -2,49 +2,69 @@
 pkgs.stdenvNoCC.mkDerivation rec {
   name = "hyprcursor-phinger";
   version = "v2.0";
-  src = builtins.fetchurl {
-    url = "https://github.com/phisch/phinger-cursors/releases/download/${version}/phinger-cursors-variants.tar.bz2";
-    sha256 = "sha256:1cg9siq1xmjb9rw01c88zqzbg4bvsibf5391g7m01yrlswd82p83";
+  src = builtins.fetchGit {
+    url = "https://github.com/phisch/phinger-cursors";
+    rev = "a7c88739be30a69610b828686a5f00f23095a031";
   };
-  nativeBuildInputs = with pkgs; [hyprcursor xcur2png];
+  nativeBuildInputs = with pkgs; [hyprcursor xcur2png jq ripgrep];
   phases = ["unpackPhase" "installPhase"];
-  unpackPhase = ''
-    runHook preUnpack
-
-    # contains multiple directories at the top-level, so default unpackPhase won't work
-    tar xf "$src";
-
-    runHook postUnpack
-  '';
   installPhase = ''
     runHook preInstall
 
-    mkdir -p $out/cursors/
-    mkdir ./phinger_extracted
+    mkdir -p ./hyprcursor/{dark/cursors_dark,light/cursors_light}
+    CURSORDIR="$(pwd ./hyprcursor)/hyprcursor"
 
-    for i in $(find -mindepth 1 -maxdepth 1 -type d); do
+    pushd theme
 
-      local theme="$(echo $i | cut -d/ -f2)"
+    # for every theme listed in cursor-theme.json
+    for i in $(seq 0 "$(( $(jq '.variants | length' cursor-theme.json) - 1 ))"); do
 
-      if [[ "$theme" == "phinger_extracted" ]]; then
-        continue
-      fi
-
-      # extract xcursor files
-      hyprcursor-util --extract "$i" --output ./phinger_extracted;
+      local currentTheme="$(jq -r ".variants[$i].name" cursor-theme.json)"
 
       # generate Hyprcursor manifest
       echo -en "
-      name = $theme
-      description =  Most likely the most over engineered cursor theme.
-      version = ${version}
-      cursors_directory = hyprcursors
-      " > "phinger_extracted/extracted_$theme/manifest.hl"
+    name = phinger-cursors-$currentTheme
+    description = Most likely the most over engineered cursor theme.
+    version = ${version}
+    cursors_directory = cursors_$currentTheme
+      " > "$CURSORDIR/$currentTheme/manifest.hl"
 
-      # compile Hyprcursor theme
-      hyprcursor-util --create "phinger_extracted/extracted_$theme/" --output "$out/cursors"
+      # loop over all the cursors of this theme & create meta.hl for every single one
+      for c in $(seq 0 "$(( $(jq ".variants[$i].cursors | length" cursor-theme.json) - 1 ))"); do
+
+        cursorName="$(jq -r ".variants[$i].cursors[$c].name" cursor-theme.json)"
+
+        # cat & rg the first SVG in sprites[] to get the hotspot
+        hotspot_x="$(cat "$(jq -r ".variants[$i].cursors[$c].sprites[0].file" cursor-theme.json)" | rg "<rect id=\"center\"" | rg -Po '(?<=x=\")([0-9]*)' || echo 0)"
+        hotspot_y="$(cat "$(jq -r ".variants[$i].cursors[$c].sprites[0].file" cursor-theme.json)" | rg "<rect id=\"center\"" | rg -Po '(?<=y=\")([0-9]*)' || echo 0)"
+
+        # get the SVG filename without the [light|dark]/ prefix
+        cursorFile="$(jq -r ".variants[0].cursors[0].sprites[0].file" cursor-theme.json | cut -d/ -f2)"
+
+        # copy the SVG to CURSORDIR
+        mkdir -p "$CURSORDIR/$currentTheme/cursors_$currentTheme/$cursorName"
+        cp "$currentTheme/$cursorFile" "$CURSORDIR/$currentTheme/cursors_$currentTheme/$cursorName/"
+
+        # create meta.hl for this SVG under CURSORDIR
+        echo -en "
+    resize_algorithm = bilinear
+    hotspot_x = $hotspot_x
+    hotspot_y = $hotspot_y
+    define_override = $cursorName
+    define_size = 24, $cursorFile
+        " > "$CURSORDIR/$currentTheme/cursors_$currentTheme/$cursorName/meta.hl"
+
+      done
+
+      # make sure output dir exists
+      mkdir -p "$out/cursors/phinger-cursors-$currentTheme/"
+
+      # compile current Hyprcursor theme
+      hyprcursor-util --create "$CURSORDIR/$currentTheme/" --output "$out/cursors"
 
     done;
+
+    popd
 
     runHook postInstall
   '';
