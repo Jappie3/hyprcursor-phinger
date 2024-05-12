c655715e1113298a5f1f977e1fee9e7d41e24ad1
Jappie3 <jasper22034@gmail.com>
2024-05-13 00:44:20 +0200

    feat: port Nix changes to standalone bash script

--- phinger-to-hyprcursor.sh
+++ phinger-to-hyprcursor.sh
@@ -1,40 +1,70 @@
 #!/usr/bin/env bash
 
+# dependencies: hyprcursor-util, rg, jq, wget
+
 version="v2.0"
-url="https://github.com/phisch/phinger-cursors/releases/download/$version/phinger-cursors-variants.tar.bz2";
-tarball="phinger-$version.tar.bz2"
+commit="a7c88739be30a69610b828686a5f00f23095a031"
+url="https://github.com/phisch/phinger-cursors/archive/$commit.tar.gz"
 out="/tmp/hyprcursor-phinger"
 
 mkdir -p "$out"
 pushd "$out"
 
-wget -O "$tarball" "$url"
-tar xf "$tarball"
-rm "$tarball"
+wget "$url"
+tar xzf "$commit.tar.gz" --directory /tmp
+cp -r "/tmp/phinger-cursors-$commit/theme" ./
 
-mkdir -p "$out/cursors/" "$out/extracted/"
 
-for i in $(find -mindepth 1 -maxdepth 1 -type d); do
+mkdir -p ./hyprcursor/{dark/cursors_dark,light/cursors_light}
+CURSORDIR="$(pwd ./hyprcursor)/hyprcursor"
 
-    theme="$(echo $i | cut -d/ -f2)"
+pushd theme
 
-    if [[ "$theme" == "extracted" ]] || [[ "$theme" == "cursors" ]]; then
-        continue
-    fi
+# for every theme listed in cursor-theme.json
+for i in $(seq 0 "$(( $(jq '.variants | length' cursor-theme.json) - 1 ))"); do
 
-    # extract xcursor files
-    hyprcursor-util --extract "$i" --output "$out/extracted/"
+    currentTheme="$(jq -r ".variants[$i].name" cursor-theme.json)"
 
     # generate Hyprcursor manifest
     echo -en "
-    name = $theme
-    description =  Most likely the most over engineered cursor theme.
-    version = ${version}
-    cursors_directory = hyprcursors
-    " > "$out/extracted/extracted_$theme/manifest.hl"
+name = phinger-cursors-$currentTheme
+description = Most likely the most over engineered cursor theme.
+version = ${version}
+cursors_directory = cursors_$currentTheme
+    " > "$CURSORDIR/$currentTheme/manifest.hl"
 
-    # compile Hyprcursor theme
-    hyprcursor-util --create "$out/extracted/extracted_$theme/" --output "$out/cursors"
+    # loop over all the cursors of this theme & create meta.hl for every single one
+    for c in $(seq 0 "$(( $(jq ".variants[$i].cursors | length" cursor-theme.json) - 1 ))"); do
+
+    cursorName="$(jq -r ".variants[$i].cursors[$c].name" cursor-theme.json)"
+
+    # cat & rg the first SVG in sprites[] to get the hotspot
+    hotspot_x="$(cat "$(jq -r ".variants[$i].cursors[$c].sprites[0].file" cursor-theme.json)" | rg "<rect id=\"center\"" | rg -Po '(?<= x=\")([0-9]*)' || echo 0)"
+    hotspot_y="$(cat "$(jq -r ".variants[$i].cursors[$c].sprites[0].file" cursor-theme.json)" | rg "<rect id=\"center\"" | rg -Po '(?<= y=\")([0-9]*)' || echo 0)"
+
+    # get the SVG filename without the [light|dark]/ prefix
+    cursorFile="$(jq -r ".variants[0].cursors[0].sprites[0].file" cursor-theme.json | cut -d/ -f2)"
+
+    # copy the SVG to CURSORDIR
+    mkdir -p "$CURSORDIR/$currentTheme/cursors_$currentTheme/$cursorName"
+    cp "$currentTheme/$cursorFile" "$CURSORDIR/$currentTheme/cursors_$currentTheme/$cursorName/"
+
+    # create meta.hl for this SVG under CURSORDIR
+    echo -en "
+resize_algorithm = bilinear
+hotspot_x = $hotspot_x
+hotspot_y = $hotspot_y
+define_override = $cursorName
+define_size = 24, $cursorFile
+    " > "$CURSORDIR/$currentTheme/cursors_$currentTheme/$cursorName/meta.hl"
+
+    done
+
+    # make sure output dir exists
+    mkdir -p "$out/cursors/"
+
+    # compile current Hyprcursor theme
+    hyprcursor-util --create "$CURSORDIR/$currentTheme/" --output "$out/cursors"
 
 done;
 
